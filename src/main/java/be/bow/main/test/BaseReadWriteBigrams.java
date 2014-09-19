c630db69012d992708d51912a326a2c3f138f393
Koen Deschacht <koendeschacht@gmail.com>
2014-09-19 16:26:54 +0200

    Improved BaseReadWriteBigrams test

--- src/main/java/be/bow/main/test/BaseReadWriteBigrams.java
+++ src/main/java/be/bow/main/test/BaseReadWriteBigrams.java
@@ -7,26 +7,24 @@ import be.bow.db.DataInterface;
 import be.bow.db.DataInterfaceFactory;
 import be.bow.db.DatabaseCachingType;
 import be.bow.db.filedb.FileDataInterfaceFactory;
+import be.bow.db.kyoto.KyotoDataInterfaceFactory;
 import be.bow.db.leveldb.LevelDBDataInterfaceFactory;
-import be.bow.db.memory.InMemoryDataInterfaceFactory;
-import be.bow.db.remote.RemoteDatabaseInterfaceFactory;
-import be.bow.text.WordIterator;
+import be.bow.db.rocksdb.RocksDBDataInterfaceFactory;
 import be.bow.ui.UI;
+import be.bow.util.NumUtils;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.lang3.mutable.MutableLong;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 public abstract class BaseReadWriteBigrams extends BaseSpeedTest {
 
-    private static final long CHARS_TO_READ = 100 * 1024 * 1024;
-    private static final File largeTextFile = new File("/home/koen/bow/data/wikipedia/nlwiki-20140113-pages-articles.xml");
+    private static final int MILLION_ITEMS_TO_PROCESS = 1024;
+
+    private static final File largeTextFile = new File("/home/koen/bow/data/wikipedia/enwiki-latest-pages-articles.xml");
+    //    private static final File largeTextFile = new File("/home/koen/bow/data/wikipedia/nlwiki-20140113-pages-articles.xml");
     private static final File tmpDbDir = new File("/tmp/testDatabaseSpeed");
 
     @Autowired
@@ -38,26 +36,24 @@ public abstract class BaseReadWriteBigrams extends BaseSpeedTest {
 
     public void run() {
         try {
-            warmupTextFile(largeTextFile);
             prepareTmpDir(tmpDbDir);
-            LevelDBDataInterfaceFactory levelDBDataInterfaceFactory = new LevelDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/levelDB");
-            FileDataInterfaceFactory fileDataInterfaceFactory = new FileDataInterfaceFactory(openFilesManager, cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/fileDb");
-            RemoteDatabaseInterfaceFactory remoteDatabaseInterfaceFactory = new RemoteDatabaseInterfaceFactory(cachesManager, memoryManager, "localhost", 1208);
-            InMemoryDataInterfaceFactory inMemoryDataInterfaceFactory = new InMemoryDataInterfaceFactory(cachesManager, memoryManager);
 
-            List<TestResult> testResults = new ArrayList<>();
+            UI.write("Testing batch writing / reading");
+            testBatchWritingAndReading(new LevelDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/levelDB"), DatabaseCachingType.DIRECT);
+            testBatchWritingAndReading(new FileDataInterfaceFactory(openFilesManager, cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/fileDb"), DatabaseCachingType.CACHED_AND_BLOOM);
+            testBatchWritingAndReading(new KyotoDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/kyotoDB"), DatabaseCachingType.DIRECT);
+            testBatchWritingAndReading(new RocksDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/rocksBD", false), DatabaseCachingType.DIRECT);
+            testBatchWritingAndReading(new RocksDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/rocksBD", true), DatabaseCachingType.DIRECT);
+//            testBatchWritingAndReading(new LMDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/lmDB"), DatabaseCachingType.DIRECT);
 
-            for (int numberOfThreads = 1; numberOfThreads <= 64; numberOfThreads *= 2) {
-                doTest(testResults, levelDBDataInterfaceFactory, DatabaseCachingType.DIRECT, largeTextFile, numberOfThreads);
-                doTest(testResults, fileDataInterfaceFactory, DatabaseCachingType.CACHED_AND_BLOOM, largeTextFile, numberOfThreads);
-                doTest(testResults, remoteDatabaseInterfaceFactory, DatabaseCachingType.CACHED_AND_BLOOM, largeTextFile, numberOfThreads);
-                doTest(testResults, inMemoryDataInterfaceFactory, DatabaseCachingType.DIRECT, largeTextFile, numberOfThreads);
-            }
+            UI.write("Testing mixed writing / reading");
+            testMixedWritingReading(new LevelDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/levelDB"), DatabaseCachingType.DIRECT);
+            testMixedWritingReading(new FileDataInterfaceFactory(openFilesManager, cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/fileDb"), DatabaseCachingType.CACHED_AND_BLOOM);
+            testMixedWritingReading(new KyotoDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/kyotoDB"), DatabaseCachingType.DIRECT);
+            testMixedWritingReading(new RocksDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/rocksBD", false), DatabaseCachingType.DIRECT);
+            testMixedWritingReading(new RocksDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/rocksBD", true), DatabaseCachingType.DIRECT);
+//            testMixedWritingReading(new LMDBDataInterfaceFactory(cachesManager, memoryManager, tmpDbDir.getAbsolutePath() + "/lmDB"), DatabaseCachingType.DIRECT);
 
-            printTestResults(testResults);
-            levelDBDataInterfaceFactory.close();
-            fileDataInterfaceFactory.close();
-            remoteDatabaseInterfaceFactory.close();
         } catch (Exception exp) {
             throw new RuntimeException(exp);
         }
@@ -74,108 +70,56 @@ public abstract class BaseReadWriteBigrams extends BaseSpeedTest {
         }
     }
 
-    private static void warmupTextFile(File largeTextFile) throws IOException {
-        UI.write("Warming up text file");
-        BufferedReader rdr = new BufferedReader(new FileReader(largeTextFile));
-        long charsRead = 0;
-        while (charsRead < CHARS_TO_READ * 2) {
-            charsRead += rdr.readLine().length();
+    private void testBatchWritingAndReading(DataInterfaceFactory factory, DatabaseCachingType type) throws InterruptedException, FileNotFoundException {
+        for (long items = 1024 * 1024; items <= MILLION_ITEMS_TO_PROCESS * 1024 * 1024; items *= 2) {
+            testBatchWritingAndReading(factory, type, largeTextFile, 8, items);
         }
-        rdr.close();
-        UI.write("Finished warming up text file");
+        factory.close();
     }
 
-    private static void printTestResults(List<TestResult> testResults) {
-        for (TestResult testResult : testResults) {
-            UI.write(testResult.getFactory().getClass().getSimpleName() + " " + testResult.getType() + " " + testResult.getAvgWrite() + " " + testResult.getAvgRead());
-        }
-    }
-
-    public void doTest(List<TestResult> testResults, DataInterfaceFactory factory, DatabaseCachingType type, File largeTextFile, int numberOfThreads) throws InterruptedException, IOException {
-        testResults.add(testWritingAndReading(factory, type, largeTextFile, numberOfThreads));
-    }
-
-    public TestResult testWritingAndReading(DataInterfaceFactory factory, DatabaseCachingType type, File largeTextFile, int numberOfThreads) throws InterruptedException, FileNotFoundException {
-        UI.write("Starting tests for " + type + " " + factory.getClass().getSimpleName() + " with " + numberOfThreads + " threads.");
+    private void testBatchWritingAndReading(DataInterfaceFactory factory, DatabaseCachingType type, File largeTextFile, int numberOfThreads, final long numberOfItems) throws FileNotFoundException, InterruptedException {
         final DataInterface dataInterface = createDataInterface(type, factory);
         dataInterface.dropAllData();
-        final MutableInt numOfWrites = new MutableInt(0);
-        final MutableInt charsReadForWrite = new MutableInt(0);
-        final long startOfWrite = System.currentTimeMillis();
-        final CountDownLatch countDownLatchWrites = new CountDownLatch(numberOfThreads);
         final BufferedReader rdr = new BufferedReader(new FileReader(largeTextFile));
-        for (int i = 0; i < numberOfThreads; i++) {
-            new Thread() {
-                public void run() {
-                    char[] textBuffer = new char[200000];
-                    try {
-                        while (charsReadForWrite.longValue() < CHARS_TO_READ) {
-                            int charsRead;
-                            synchronized (rdr) {
-                                charsRead = rdr.read(textBuffer);
-                                charsReadForWrite.setValue(charsReadForWrite.longValue() + charsRead);
-                            }
-                            char[] actualTextBuffer = charsRead < textBuffer.length ? Arrays.copyOf(textBuffer, charsRead) : textBuffer;
-                            WordIterator wordIterator = new WordIterator(actualTextBuffer, Collections.<String>emptySet());
-                            String prev = null;
-                            while (wordIterator.hasNext()) {
-                                String word = wordIterator.next().toString().toLowerCase();
-                                doWrite(prev, word, dataInterface);
-                                numOfWrites.increment();
-                                prev = word;
-                            }
-                        }
-                    } catch (Exception exp) {
-                        UI.writeError("Received exception in writing thread ", exp);
-                    }
-                    countDownLatchWrites.countDown();
+        double writesPerSecond = readTextWithThreads(numberOfThreads, numberOfItems, dataInterface, rdr, 0.0).getWritesPerSecond();
+        dataInterface.optimizeForReading();
+        double readsPerSecond = readTextWithThreads(numberOfThreads, numberOfItems, dataInterface, rdr, 1.0).getReadsPerSecond();
+        dataInterface.close();
+        UI.write(factory.getClass().getSimpleName() + " threads " + numberOfThreads + " items " + numberOfItems + " write " + NumUtils.fmt(writesPerSecond) + " read " + NumUtils.fmt(readsPerSecond));
+    }
 
-                }
-            }.start();
+    private void testMixedWritingReading(DataInterfaceFactory factory, DatabaseCachingType type) throws InterruptedException, FileNotFoundException {
+        for (long items = 1024 * 1024; items <= MILLION_ITEMS_TO_PROCESS * 1024 * 1024; items *= 2) {
+            testMixedWritingReading(factory, type, largeTextFile, 8, items);
+        }
+        factory.close();
+    }
+
+    private void testMixedWritingReading(DataInterfaceFactory factory, DatabaseCachingType type, File largeTextFile, int numberOfThreads, final long numberOfItems) throws FileNotFoundException, InterruptedException {
+        final DataInterface dataInterface = createDataInterface(type, factory);
+        dataInterface.dropAllData();
+        final BufferedReader rdr = new BufferedReader(new FileReader(largeTextFile));
+        ReadTextResults readTextResults = readTextWithThreads(numberOfThreads, numberOfItems, dataInterface, rdr, 0.5);
+        double writesPerSecond = readTextResults.getWritesPerSecond();
+        double readsPerSecond = readTextResults.getReadsPerSecond();
+        dataInterface.close();
+        UI.write(factory.getClass().getSimpleName() + " threads " + numberOfThreads + " items " + numberOfItems + " write " + NumUtils.fmt(writesPerSecond) + " read " + NumUtils.fmt(readsPerSecond));
+    }
+
+    private ReadTextResults readTextWithThreads(int numberOfThreads, long numberOfItems, DataInterface dataInterface, BufferedReader rdr, double fractionToRead) throws InterruptedException {
+        final MutableLong numberOfItemsWritten = new MutableLong(0);
+        final MutableLong numberOfItemsRead = new MutableLong(0);
+        final MutableLong timeSpendWriting = new MutableLong(0);
+        final MutableLong timeSpendReading = new MutableLong(0);
+        final CountDownLatch countDownLatchWrites = new CountDownLatch(numberOfThreads);
+        for (int i = 0; i < numberOfThreads; i++) {
+            new ReadTextThread(this, numberOfItemsWritten, numberOfItemsRead, timeSpendWriting, timeSpendReading, numberOfItems, rdr, dataInterface, countDownLatchWrites, fractionToRead).start();
         }
         countDownLatchWrites.await();
         dataInterface.flush();
-        UI.write("Write phase finished, starting read phase");
-        long taken = (System.currentTimeMillis() - startOfWrite);
-        double avgWrite = taken / (double) numOfWrites.intValue();
-        final long startOfRead = System.currentTimeMillis();
-        final MutableInt numOfReads = new MutableInt(0);
-        final MutableInt charsReadForReads = new MutableInt(0);
-        final CountDownLatch countDownLatchReads = new CountDownLatch(numberOfThreads);
-        for (int i = 0; i < numberOfThreads; i++) {
-            new Thread() {
-                public void run() {
-                    char[] textBuffer = new char[200000];
-                    try {
-                        while (charsReadForReads.longValue() < CHARS_TO_READ) {
-                            int charsRead;
-                            synchronized (rdr) {
-                                charsRead = rdr.read(textBuffer);
-                                charsReadForReads.setValue(charsReadForReads.longValue() + charsRead);
-                            }
-                            char[] actualTextBuffer = charsRead < textBuffer.length ? Arrays.copyOf(textBuffer, charsRead) : textBuffer;
-                            WordIterator wordIterator = new WordIterator(actualTextBuffer, Collections.<String>emptySet());
-                            String prev = null;
-                            while (wordIterator.hasNext()) {
-                                String word = wordIterator.next().toString().toLowerCase();
-                                doRead(prev, word, dataInterface);
-                                numOfReads.increment();
-                                prev = word;
-                            }
-                        }
-                    } catch (Exception exp) {
-                        UI.writeError("Received exception in reading thread ", exp);
-                    }
-                    countDownLatchReads.countDown();
-                }
-            }.start();
-        }
-        countDownLatchReads.await();
-        taken = (System.currentTimeMillis() - startOfRead);
-        double avgRead = taken / (double) numOfReads.intValue();
-        UI.write("Finished read phase, closing data interface");
-        dataInterface.close();
-        return new TestResult(type, factory, numberOfThreads, avgRead, avgWrite);
+        double readsPerSecond = numberOfItemsRead.longValue() * 1e9 / timeSpendReading.longValue();
+        double writesPerSecond = numberOfItemsWritten.longValue() * 1e9 / timeSpendWriting.longValue();
+        return new ReadTextResults(readsPerSecond, writesPerSecond);
     }
 
     protected abstract DataInterface createDataInterface(DatabaseCachingType type, DataInterfaceFactory factory);
@@ -184,41 +128,22 @@ public abstract class BaseReadWriteBigrams extends BaseSpeedTest {
 
     protected abstract void doWrite(String prev, String word, DataInterface dataInterface);
 
-    private static class TestResult {
-        private final DatabaseCachingType type;
-        private final DataInterfaceFactory factory;
-        private final int numberOfThreads;
-        private final double avgRead;
-        private final double avgWrite;
+    private static class ReadTextResults {
+        private double readsPerSecond;
+        private double writesPerSecond;
 
-        private TestResult(DatabaseCachingType type, DataInterfaceFactory factory, int numberOfThreads, double avgRead, double avgWrite) {
-            this.type = type;
-            this.factory = factory;
-            this.avgRead = avgRead;
-            this.avgWrite = avgWrite;
-            this.numberOfThreads = numberOfThreads;
+        private ReadTextResults(double readsPerSecond, double writesPerSecond) {
+            this.readsPerSecond = readsPerSecond;
+            this.writesPerSecond = writesPerSecond;
         }
 
-        private DatabaseCachingType getType() {
-            return type;
+        public double getReadsPerSecond() {
+            return readsPerSecond;
         }
 
-        private DataInterfaceFactory getFactory() {
-            return factory;
-        }
-
-        private double getAvgRead() {
-            return avgRead;
-        }
-
-        private double getAvgWrite() {
-            return avgWrite;
-        }
-
-        public int getNumberOfThreads() {
-            return numberOfThreads;
+        public double getWritesPerSecond() {
+            return writesPerSecond;
         }
     }
-
 
 }
--- src/main/java/be/bow/main/test/ReadTextThread.java
+++ src/main/java/be/bow/main/test/ReadTextThread.java
@@ -3,12 +3,14 @@ package be.bow.main.test;
 import be.bow.db.DataInterface;
 import be.bow.text.WordIterator;
 import be.bow.util.SafeThread;
-import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.lang3.mutable.MutableLong;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by Koen Deschacht (koendeschacht@gmail.com) on 9/17/14.
@@ -16,25 +18,34 @@ import java.util.Collections;
 class ReadTextThread extends SafeThread {
 
     private BaseReadWriteBigrams baseReadWriteBigrams;
-    private final MutableInt numberOfItemsWritten;
+    private final MutableLong numberOfItemsWritten;
+    private final MutableLong numberOfItemsRead;
+    private final MutableLong timeSpendWriting;
+    private final MutableLong timeSpendReading;
     private final long numberOfItems;
-    private final long startTime;
     private final BufferedReader rdr;
     private final DataInterface dataInterface;
+    private CountDownLatch countDownLatch;
+    private final double fractionToRead;
 
-    public ReadTextThread(BaseReadWriteBigrams baseReadWriteBigrams, MutableInt numberOfItemsWritten, long numberOfItems, long startTime, BufferedReader rdr, DataInterface dataInterface) {
+    public ReadTextThread(BaseReadWriteBigrams baseReadWriteBigrams, MutableLong numberOfItemsWritten, MutableLong numberOfItemsRead, MutableLong timeSpendWriting, MutableLong timeSpendReading, long numberOfItems, BufferedReader rdr, DataInterface dataInterface, CountDownLatch countDownLatch, double fractionToRead) {
         super("ReadTextThread", false);
         this.baseReadWriteBigrams = baseReadWriteBigrams;
         this.numberOfItemsWritten = numberOfItemsWritten;
+        this.numberOfItemsRead = numberOfItemsRead;
+        this.timeSpendReading = timeSpendReading;
+        this.timeSpendWriting = timeSpendWriting;
         this.numberOfItems = numberOfItems;
-        this.startTime = startTime;
         this.rdr = rdr;
         this.dataInterface = dataInterface;
+        this.countDownLatch = countDownLatch;
+        this.fractionToRead = fractionToRead;
     }
 
     public void runInt() throws IOException {
+        Random random = new Random();
         char[] textBuffer = new char[200000];
-        while (numberOfItemsWritten.longValue() < numberOfItems && (System.currentTimeMillis() - startTime) < BaseReadWriteBigrams.MAX_TIME) {
+        while (numberOfItemsWritten.longValue() + numberOfItemsRead.longValue() < numberOfItems) {
             int charsRead;
             synchronized (rdr) {
                 charsRead = rdr.read(textBuffer);
@@ -42,19 +53,30 @@ class ReadTextThread extends SafeThread {
             if (charsRead < 0) {
                 throw new RuntimeException("Could not read the text");
             }
+            boolean readValuesInThisText = random.nextDouble() < fractionToRead;
+            long startTime = System.nanoTime();
             long numberOfItemsWrittenForThisText = 0;
             char[] actualTextBuffer = charsRead < textBuffer.length ? Arrays.copyOf(textBuffer, charsRead) : textBuffer;
             WordIterator wordIterator = new WordIterator(actualTextBuffer, Collections.<String>emptySet());
             String prev = null;
             while (wordIterator.hasNext()) {
                 String word = wordIterator.next().toString().toLowerCase();
-                baseReadWriteBigrams.doWrite(prev, word, dataInterface);
+                if (readValuesInThisText) {
+                    baseReadWriteBigrams.doRead(prev, word, dataInterface);
+                } else {
+                    baseReadWriteBigrams.doWrite(prev, word, dataInterface);
+                }
                 numberOfItemsWrittenForThisText++;
                 prev = word;
             }
-            synchronized (numberOfItemsWritten) {
-                numberOfItemsWritten.setValue(numberOfItemsWritten.longValue() + numberOfItemsWrittenForThisText);
+            long endTime = System.nanoTime();
+            MutableLong itemsWritten = readValuesInThisText ? numberOfItemsRead : numberOfItemsWritten;
+            MutableLong timeSpend = readValuesInThisText ? timeSpendReading : timeSpendWriting;
+            synchronized (itemsWritten) {
+                itemsWritten.setValue(itemsWritten.longValue() + numberOfItemsWrittenForThisText);
+                timeSpend.setValue(timeSpend.longValue() + (endTime - startTime));
             }
         }
+        countDownLatch.countDown();
     }
 }
