1af31000a370a6fb1ae93cf8f9613fbc6bcc4c70
Koen Deschacht <koendeschacht@gmail.com>
2014-09-19 16:25:46 +0200

    improved BloomFilterDataInterface

--- src/main/java/be/bow/db/bloomfilter/BloomFilterDataInterface.java
+++ src/main/java/be/bow/db/bloomfilter/BloomFilterDataInterface.java
@@ -8,24 +8,107 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class BloomFilterDataInterface<T extends Object> extends LayeredDataInterface<T> {
 
-    private static final double INITIAL_FPP = 0.01;
-    private final static double MAX_FPP = INITIAL_FPP * 2;
+    private static final double INITIAL_FPP = 0.001;
+    private final static double MAX_FPP = INITIAL_FPP * 20;
     private final DataInterface<LongBloomFilterWithCheckSum> bloomFilterDataInterface;
+    private final ReentrantLock modifyBloomFilterLock;
     private LongBloomFilterWithCheckSum bloomFilter;
-    private ReentrantLock modifyBloomFilterLock;
+    private boolean bloomFilterWasWrittenToDisk;
     private long currentKeyForNewBloomFilterCreation = Long.MAX_VALUE;
+
+    private long timeOfLastRead;
 
     public BloomFilterDataInterface(DataInterface<T> baseInterface, DataInterface<LongBloomFilterWithCheckSum> bloomFilterDataInterface) {
         super(baseInterface);
         this.bloomFilterDataInterface = bloomFilterDataInterface;
         this.modifyBloomFilterLock = new ReentrantLock();
+        this.bloomFilter = bloomFilterDataInterface.read(getName());
     }
 
-    private void createNewBloomFilter() {
-        baseInterface.flush(); //Make sure all values are written to disk
-        long numOfValuesForBloomFilter = Math.max(1000, baseInterface.apprSize());
-//        UI.write("Creating bloomfilter " + getName());
+    @Override
+    public void optimizeForReading() {
+        if (bloomFilter == null) {
+            createNewBloomFilter();
+        }
+        baseInterface.optimizeForReading();
+    }
+
+    @Override
+    public T readInt(long key) {
+        timeOfLastRead = System.currentTimeMillis();
+        LongBloomFilterWithCheckSum currentBloomFilter = bloomFilter;
+        if (currentBloomFilter == null && modifyBloomFilterLock.tryLock()) {
+            createNewBloomFilter();
+            currentBloomFilter = bloomFilter;
+            modifyBloomFilterLock.unlock();
+        }
+        if (currentBloomFilter == null || currentKeyForNewBloomFilterCreation < key) {
+            //we are still creating the bloom filter
+            return baseInterface.read(key);
+        } else {
+            if (currentBloomFilter.mightContain(key)) {
+                return baseInterface.read(key);
+            } else {
+                return null;
+            }
+        }
+    }
+
+
+    @Override
+    public void dropAllData() {
+        modifyBloomFilterLock.lock();
+        try {
+            baseInterface.dropAllData();
+            createNewBloomFilterNonSynchronized();
+        } finally {
+            modifyBloomFilterLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean mightContain(long key) {
+        LongBloomFilterWithCheckSum currentBloomFilter = bloomFilter;
+        if (currentBloomFilter == null && modifyBloomFilterLock.tryLock()) {
+            createNewBloomFilter();
+            currentBloomFilter = bloomFilter;
+            modifyBloomFilterLock.unlock();
+        }
+        if (currentBloomFilter == null || currentKeyForNewBloomFilterCreation < key) {
+            //we are still creating the bloom filter
+            return baseInterface.mightContain(key);
+        } else {
+            return currentBloomFilter.mightContain(key);
+        }
+    }
+
+    @Override
+    public void valuesChanged(long[] keys) {
+        super.valuesChanged(keys);
+        modifyBloomFilterLock.lock();
+        if (bloomFilter != null) {
+            if (keys.length > 0) {
+                bloomFilterWasWrittenToDisk = false;
+            }
+            for (Long key : keys) {
+                bloomFilter.put(key);
+            }
+            if (bloomFilter.expectedFpp() > MAX_FPP) {
+                if (System.currentTimeMillis() - timeOfLastRead < 1000) {
+                    //read in last second, create a new bloom filter
+                    createNewBloomFilterNonSynchronized();
+                } else {
+                    //no reads in last second, let's drop the bloom filter for now
+                    bloomFilter = null;
+                }
+            }
+        }
+        modifyBloomFilterLock.unlock();
+    }
+
+    private void createNewBloomFilterNonSynchronized() {
         currentKeyForNewBloomFilterCreation = Long.MIN_VALUE;
+        long numOfValuesForBloomFilter = Math.max(1000000, baseInterface.apprSize());
         bloomFilter = new LongBloomFilterWithCheckSum(numOfValuesForBloomFilter, INITIAL_FPP);
         bloomFilter.setDataCheckSum(baseInterface.dataCheckSum());
         long start = System.currentTimeMillis();
@@ -38,85 +121,23 @@ public class BloomFilterDataInterface<T extends Object> extends LayeredDataInter
             currentKeyForNewBloomFilterCreation = key;
         }
         it.close();
-//        long taken = (System.currentTimeMillis() - start);
-//        UI.write("Created bloomfilter " + getName() + " in " + taken + " ms for " + numOfKeys + " keys.");
+        currentKeyForNewBloomFilterCreation = Long.MAX_VALUE;
+        long taken = (System.currentTimeMillis() - start);
+//        UI.write("Created bloomfilter " + getName() + " in " + taken + " ms for " + numOfKeys + " keys, size is " + bloomFilter.getBits().size() / (8 * 1024) + " kbytes.");
     }
 
-    @Override
-    public void optimizeForReading() {
-        checkInitialization(); //create bloom filter if necessary
-        baseInterface.optimizeForReading();
-    }
-
-    @Override
-    public T readInt(long key) {
-        checkInitialization();
-        if (bloomFilter == null || currentKeyForNewBloomFilterCreation < key) {
-            //we are still creating the bloom filter
-            return baseInterface.read(key);
-        } else {
-            boolean mightContainValue = true;
-            try {
-                mightContainValue = bloomFilter.mightContain(key);
-            } catch (NullPointerException exp) {
-                //bloom filter might have become null. We don't use a lock to prevent that case since that slows things down too much.
-            }
-            if (mightContainValue) {
-                return baseInterface.read(key);
-            } else {
-                return null;
-            }
-        }
-    }
-
-    private void checkInitialization() {
-        if (bloomFilter == null) {
-            if (modifyBloomFilterLock.tryLock()) {
-                bloomFilter = bloomFilterDataInterface.read(getName());
-                if (bloomFilter == null || bloomFilter.getDataCheckSum() != baseInterface.dataCheckSum()) {
-                    createNewBloomFilter();
-                    bloomFilterDataInterface.write(getName(), bloomFilter);
-                }
-                modifyBloomFilterLock.unlock();
-            }
-        }
-    }
-
-    @Override
-    public void dropAllData() {
+    private void createNewBloomFilter() {
         modifyBloomFilterLock.lock();
-        try {
-            baseInterface.dropAllData();
-            bloomFilterDataInterface.remove(getName());
-            bloomFilter = null;
-        } finally {
-            modifyBloomFilterLock.unlock();
-        }
-    }
-
-    @Override
-    public boolean mightContain(long key) {
-        checkInitialization();
-        if (bloomFilter == null) {
-            //we are still creating the bloom filter
-            return baseInterface.mightContain(key);
-        } else {
-            return bloomFilter.mightContain(key);
-        }
-    }
-
-    @Override
-    public void valuesChanged(long[] keys) {
-        super.valuesChanged(keys);
-        modifyBloomFilterLock.lock();
-        if (bloomFilter != null) {
-            for (Long key : keys) {
-                bloomFilter.put(key);
-            }
-            if (bloomFilter.expectedFpp() > MAX_FPP) {
-                bloomFilter = null;
-            }
-        }
+        createNewBloomFilterNonSynchronized();
         modifyBloomFilterLock.unlock();
+    }
+
+    @Override
+    public void flush() {
+        baseInterface.flush();
+        if (!bloomFilterWasWrittenToDisk) {
+            bloomFilterDataInterface.write(getName(), bloomFilter);
+            bloomFilterWasWrittenToDisk = true;
+        }
     }
 }
