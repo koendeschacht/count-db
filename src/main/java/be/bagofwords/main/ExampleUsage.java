468c060736bdb5fc6e60f93b6a09e6ac9e36cf24
Koen Deschacht <koendeschacht@gmail.com>
2014-10-01 12:17:33 +0200

    Expanded example usage

--- src/main/java/be/bagofwords/main/ExampleUsage.java
+++ src/main/java/be/bagofwords/main/ExampleUsage.java
@@ -5,6 +5,8 @@ import be.bagofwords.db.DataInterfaceFactory;
 import be.bagofwords.db.DatabaseCachingType;
 import be.bagofwords.db.application.EmbeddedDBContextFactory;
 import be.bagofwords.db.combinator.OverWriteCombinator;
+import be.bagofwords.iterator.CloseableIterator;
+import be.bagofwords.util.KeyValue;
 import org.apache.commons.lang3.time.DateUtils;
 
 import java.text.ParseException;
@@ -19,24 +21,37 @@ public class ExampleUsage {
         //create data interface factory that stores all data in /tmp/myData (This factory is wired with spring)
         DataInterfaceFactory dataInterfaceFactory = new EmbeddedDBContextFactory("/tmp/myData").createApplicationContext().getBean(DataInterfaceFactory.class);
 
-        //create databases
+        //create data interfaces
         DataInterface<Long> myLogDataInterface = dataInterfaceFactory.createCountDataInterface("myLoginCounts");
         DataInterface<UserObject> myUserDataInterface = dataInterfaceFactory.createDataInterface(DatabaseCachingType.CACHED, "myUsers", UserObject.class, new OverWriteCombinator<UserObject>());
 
         //write data
-        int userId = 12939;
+        long userId = 12939;
         myLogDataInterface.increaseCount("user_" + userId + "_logged_in");
         myUserDataInterface.write(userId, new UserObject("koen", "deschacht", DateUtils.parseDate("1983-04-12", "yyyy-MM-dd")));
 
-        //flush data
+        //flush data (necessary to make the written data visible on next read)
         myLogDataInterface.flush();
         myUserDataInterface.flush();
 
         //read data
         long numOfLogins = myLogDataInterface.readCount("user_" + userId + "_logged_in");
         UserObject user = myUserDataInterface.read(userId);
-
         System.out.println("User " + user.getFirstName() + " " + user.getSecondName() + " logged in " + numOfLogins + " times.");
+
+        //iterate over all data
+        CloseableIterator<KeyValue<UserObject>> iterator = myUserDataInterface.iterator();
+        while (iterator.hasNext()) {
+            KeyValue<UserObject> curr = iterator.next();
+            UserObject currUser = curr.getValue();
+            long currUserId = curr.getKey();
+            System.out.println("User " + currUser.getFirstName() + " " + currUser.getSecondName() + " with id " + currUserId);
+        }
+        iterator.close();
+
+        //drop all data
+        myLogDataInterface.dropAllData();
+        myUserDataInterface.dropAllData();
     }
 
     public static class UserObject {
